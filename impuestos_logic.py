37ac5ff89b8663703a28654c36d2bc717b244f68
SAMUEL <waprolear@gmail.com>
2026-08-04 10:53:00 -0500

    inicial logica

---
+++ impuestos_logic.py
@@ -0,0 +1,10 @@
+# impuestos_logic.py
+# Calculadora de Impuestos de Compra - Colombia
+EXENTO = "exento"
+IVA_5 = "iva5"
+IVA_19 = "iva19"
+INC_RESTAURANTES = "inc_restaurantes"
+LICORES = "licores"
+SUNTUARIOS = "suntuarios"
+BOLSAS_PLASTICAS = "bolsas"
+CIGARRILLOS_VAPEADORES = "vapeadores"
