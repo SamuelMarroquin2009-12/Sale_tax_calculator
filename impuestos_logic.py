985ccb83146daf7ca6a79bf8c6354a49a817b49c
SAMUEL <waprolear@gmail.com>
2026-08-04 10:53:58 -0500

    Logica terminada con excepciones

--- impuestos_logic.py
+++ impuestos_logic.py
@@ -8,3 +8,15 @@ LICORES = "licores"
 SUNTUARIOS = "suntuarios"
 BOLSAS_PLASTICAS = "bolsas"
 CIGARRILLOS_VAPEADORES = "vapeadores"
+# Excepciones del proyecto
+class NegativePriceError(Exception):
+    pass
+class NonNumericPriceError(Exception):
+    pass
+class ZeroOrNegativeQuantityError(Exception):
+    pass
+class NegativeQuantityError(Exception):
+    pass
+# Funcion principal encargada del calculo de la multiplicacion y extraccion del porcentaje y valor del impuesto
+def calcular_impuesto(categoria, precio_unitario, cantidad):
+    pass
